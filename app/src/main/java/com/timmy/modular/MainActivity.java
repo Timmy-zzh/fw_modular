6512bc02248dc321c328a349cd8aa057a73071ca
timmy1 <timmy_zzh@163.com>
2021-08-13 19:33:14 +0800

    Initial commit

---
+++ app/src/main/java/com/timmy/modular/MainActivity.java
@@ -0,0 +1,17 @@
+package com.timmy.modular;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 写个框架: 组件化
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
